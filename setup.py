7b49d4b7507feaa9c22a4f8a5108706d53c1883e
Arthur Valencio <33551656+artvalencio@users.noreply.github.com>
2021-11-05 22:26:11 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(name='dynamic_connect',
 version='0.01',
 description='Python package for building dynamic functional connectome from EEG',
@@ -6,7 +6,7 @@ url='https://github.com/artvalencio/dynamic-connect',
 author='Arthur Valencio, IC-Unicamp, RIDC NeuroMat',
 author_email='arthuric@unicamp.br',
 license='MIT',
-packages=['dynamic_connect'],
+packages=find_packages(),
 classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -14,5 +14,6 @@ classifiers=[
     ],
 python_requires='>=3.6',
 install_requires=['pandas','numpy','matplotlib','scipy','mne','seaborn',
-                  '<cami> @ https://github.com/artvalencio/cami-python/archive/master.zip'],
+                  'cami @ git+https://github.com/artvalencio/cami-python.git@master',
+                  ],
 zip_safe=False)
